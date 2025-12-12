76c46020038fd2f9942deaa36275bd0813dfaf05
Wonjun Jung <kokoseij@gmail.com>
2025-12-11 19:20:16 -0800

    script: reimplement functionality as class

--- scripts/script.js
+++ scripts/script.js
@@ -9,101 +9,113 @@ const hangmanInputElem = document.getElementById("hangmanInput");
 const hangmanSubmitElem = document.getElementById("hangmanSubmit");
 const hangmanAgainElem = document.getElementById("hangmanAgain");
 
-let answer = "";
-let correctChars = [];
-let incorrectChars = [];
-let errors = 0;
-
-function hangmanUpdate() {
-	let isGameOver = errors == 6;
-	let isCorrect = !isGameOver;
-
-	let showStr = Array.from(answer).reduce((accum, char) => {
-		if (char == " ") {
-			return accum + "&nbsp;&nbsp;";
-		} else if (correctChars.includes(char) || isGameOver)
-			return accum + `${char.toUpperCase()} `;
-		else {
-			isCorrect = false;
-			return accum + "_ ";
-		}
-	}, "");
-
-	if (isCorrect) isGameOver = true;
-
-	hangmanImgElem.setAttribute("src", `images/hangman${errors}.png`);
-
-	hangmanTxtElem.innerHTML = showStr;
-
-	hangmanUsedElem.innerText = incorrectChars.reduce(
-		(accum, char) => `${accum}${char.toUpperCase()} `,
-		"",
-	);
-
-	if (isGameOver) {
-		hangmanResultElem.innerHTML = isCorrect ? "You Win!" : "You Lose!";
-		hangmanResultElem.classList.remove("hidden");
-		hangmanInputDivElem.classList.add("hidden");
-		hangmanAgainElem.classList.remove("hidden");
-	}
-}
-
-function init() {
+class hangmanClass {
+	answer = "";
 	correctChars = [];
 	incorrectChars = [];
 	errors = 0;
 
-	hangmanResultElem.classList.add("hidden");
-	hangmanInputDivElem.classList.remove("hidden");
-	hangmanAgainElem.classList.add("hidden");
-
-	fetch("data/answers.json").then((resp) => {
-		if (!resp.ok) {
-			alert("Failed to receive answer data!");
-			return;
-		}
-
-		resp.json().then((data) => {
-			const answerObj = data[Math.floor(Math.random() * data.length)];
-			answer = answerObj.answer;
-			hangmanHintElem.innerHTML = `Hint: ${answerObj.hint}`;
-
-			hangmanUpdate();
-		});
-	});
-}
-
-hangmanInputElem.addEventListener("input", (e) => {
-	if (!e.data) return;
-	let userInput = e.data.replace(/[^a-z]/, "");
-	if ((correctChars + incorrectChars).includes(userInput)) userInput = "";
-
-	e.target.value = userInput.toUpperCase();
-});
-
-const submitHandler = () => {
-	let userInput = hangmanInputElem.value.toLowerCase();
-	hangmanInputElem.value = "";
-	if (userInput.length == 0) return;
-	userInput = userInput[0];
-
-	if ((correctChars + incorrectChars).includes(userInput)) return;
-
-	if (answer.includes(userInput)) {
-		correctChars.push(userInput);
-	} else {
-		incorrectChars.push(userInput);
-		errors++;
+	constructor() {
+		this.init();
 	}
 
-	hangmanUpdate();
-};
+	hangmanUpdate = () => {
+		let isGameOver = this.errors == 6;
+		let isCorrect = !isGameOver;
+
+		let showStr = Array.from(this.answer).reduce((accum, char) => {
+			if (char == " ") {
+				return accum + "&nbsp;&nbsp;";
+			} else if (this.correctChars.includes(char) || isGameOver)
+				return accum + `${char.toUpperCase()} `;
+			else {
+				isCorrect = false;
+				return accum + "_ ";
+			}
+		}, "");
+
+		if (isCorrect) isGameOver = true;
+
+		hangmanImgElem.setAttribute("src", `images/hangman${this.errors}.png`);
+
+		hangmanTxtElem.innerHTML = showStr;
+
+		hangmanUsedElem.innerText = this.incorrectChars.reduce(
+			(accum, char) => `${accum}${char.toUpperCase()} `,
+			"",
+		);
+
+		if (isGameOver) {
+			hangmanResultElem.innerHTML = isCorrect ? "You Win!" : "You Lose!";
+			hangmanResultElem.classList.remove("hidden");
+			hangmanInputDivElem.classList.add("hidden");
+			hangmanAgainElem.classList.remove("hidden");
+		}
+	}
+
+	inputCheck = (e) => {
+		if (!e.data) return;
+		let userInput = e.data.replace(/[^a-z]/, "");
+		if ((this.correctChars + this.incorrectChars).includes(userInput))
+			userInput = "";
+
+		e.target.value = userInput.toUpperCase();
+	};
+
+	submitHandler = (e) => {
+		let userInput = hangmanInputElem.value.toLowerCase();
+		hangmanInputElem.value = "";
+		if (userInput.length == 0) return;
+		userInput = userInput[0];
+
+		if ((this.correctChars + this.incorrectChars).includes(userInput))
+			return;
+
+		if (this.answer.includes(userInput)) {
+			this.correctChars.push(userInput);
+		} else {
+			this.incorrectChars.push(userInput);
+			this.errors++;
+		}
+
+		this.hangmanUpdate();
+	};
+
+	init = () => {
+		this.correctChars = [];
+		this.incorrectChars = [];
+		this.errors = 0;
+
+		hangmanResultElem.classList.add("hidden");
+		hangmanInputDivElem.classList.remove("hidden");
+		hangmanAgainElem.classList.add("hidden");
+
+		fetch("data/answers.json").then((resp) => {
+			if (!resp.ok) {
+				alert("Failed to receive answer data!");
+				return;
+			}
+
+			resp.json().then((data) => {
+				const answerObj = data[Math.floor(Math.random() * data.length)];
+				this.answer = answerObj.answer;
+				hangmanHintElem.innerHTML = `Hint: ${answerObj.hint}`;
+
+				this.hangmanUpdate();
+			});
+		});
+	}
+}
+
+const hangman = new hangmanClass();
+
+hangmanInputElem.addEventListener("input", hangman.inputCheck);
 
 hangmanInputElem.addEventListener("keyup", (e) => {
-	if (e.key == "Enter") submitHandler();
+	if (e.key == "Enter") hangman.submitHandler();
 });
-hangmanSubmitElem.addEventListener("click", submitHandler);
+hangmanSubmitElem.addEventListener("click", hangman.submitHandler);
 
-hangmanAgainElem.addEventListener("click", init);
+hangmanAgainElem.addEventListener("click", hangman.init);
 
-init();
+hangman.init();
